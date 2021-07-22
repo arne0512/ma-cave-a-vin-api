5698f06c3f49af396f047c543cf48e06872f2714
arne0512 <arn.clerc@gmail.com>
2021-07-22 23:08:20 +0200

    last modifs

--- controllers/wines.js
+++ controllers/wines.js
@@ -1,4 +1,4 @@
-const { findMany } = require("../models/wines");
+const { findMany, create, getOneWine, delete_, update } = require("../models/wines");
 
 const getWines = async (req, res) => {
   const [wines] = await findMany(req.query);
@@ -6,16 +6,42 @@ const getWines = async (req, res) => {
 };
 
 const createWine = async (req, res) => {
+  try {
+    await create(req.body);
+    res.status(201).send("Wine has been created");
+  } catch (err) {
+    res.status(500).send("Error creating wine");
+  }
+};
+
+const getWine = async (req, res) => {
+  const [wine] = await getOneWine(req.params.id);
+  res.status(200).json(wine);
+};
+
+const deleteWine = async (req, res) => {
+  try {
+    await delete_(req.params.id);
+    res.status(204).send();
+  } catch (err) {
+      console.log(err);
+    res.status(500).send("Error deleting wine");
+  }
+};
+
+const updateWine = async (req, res) => {
     try {
-      await create(req.body);
-      res.status(201).send("Wine has been created");
+      await update(req.params.id, req.body);
+      res.status(200).send("Wine has been updated");
     } catch (err) {
-        console.log(err);
-      res.status(500).send("Error creating wine");
+      res.status(500).send("Error updating wine");
     }
   };
 
 module.exports = {
   getWines,
-  createWine
+  createWine,
+  getWine,
+  deleteWine,
+  updateWine
 };
